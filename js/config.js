46ea16cc04d7013b1206a4b3de1cf746192bd2b7
yatirajshetty <yatiraj.shetty@yatirajshettys-MacBook-Pro.local>
2020-09-21 20:22:37 +0530

    adding cognito details

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_TjCmbt6Cs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5e7rvdl4r8i4v29l8a5sltne27', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
